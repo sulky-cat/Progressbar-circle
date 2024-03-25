3bb2ea90dea28e6f3b0113e11b753773015fd826
sulky-cat <sulky.cat.maxim@gmail.com>
2024-03-25 14:51:57 +0300

    Add demo + src

---
+++ demo/script.js
@@ -0,0 +1,121 @@
+import ProgressBar from "./../src/ProgressBar.js";
+
+/**
+ * min +
+ * max +
+ * current +
+ * clockwise +
+ * angle +
+ * 
+ * animation duration +
+ * animation trigger +
+ * animation cb +
+ * animation timeFunc https://gizma.com/easing/ +
+ * 
+ * text +
+ * text formatText +
+ */
+
+// Example 1 Стандартное поведените
+const ex_1_render = document.querySelector('.ex-1')
+new ProgressBar(ex_1_render)
+// Example 2 Без анимации
+const ex_2_render = document.querySelector('.ex-2')
+new ProgressBar(ex_2_render, {
+   animation: false,
+})
+// Example 3 Без циф
+const ex_3_render = document.querySelector('.ex-3')
+new ProgressBar(ex_3_render, {
+   text: false,
+   max: 10,
+   current: 9
+})
+// Example 4 Отрицательный диапазон
+const ex_4_render = document.querySelector('.ex-4')
+new ProgressBar(ex_4_render)
+// Example 5 Другие настрйоки анимации
+const ex_5_render = document.querySelector('.ex-5')
+const ex_5_cb = document.createElement('div')
+ex_5_cb.classList.add('test')
+ex_5_cb.innerHTML = 'Вывод callback функции. Current: <span></span>'
+ex_5_render.parentElement.append(ex_5_cb)
+new ProgressBar(ex_5_render, {
+   min: 0,
+   max: 100,
+   current: 77,
+   animation: {
+      duration: 3000,
+      timeFunc: (time, current, duration, min) => {
+         // easeInOutQuad https://gizma.com/easing/#easeInOutQuad
+         time /= duration / 2;
+         if (time < 1) return current / 2 * time * time + min;
+         time--;
+         return -current / 2 * (time * (time - 2) - 1) + min;
+      },
+      cb: (current) => {
+         ex_5_cb.children[0].innerHTML = current.toFixed(2)
+      },
+   }
+})
+// Example 6 Анимация по триггеру
+const ex_6_render = document.querySelector('.ex-6')
+document.querySelector('.section_button_ex-6').onclick = () => {
+   ex_6.animation({})
+}
+const ex_6 = new ProgressBar(ex_6_render, {
+   current: 45,
+   animation: { trigger: true }
+})
+// Example 7 Другой угол начала + направление
+const ex_7_render = document.querySelector('.ex-7')
+new ProgressBar(ex_7_render, {
+   current: 6,
+   angle: 0,
+   max: 6,
+   clockwise: false,
+   animation: {
+      timeFunc: function (time, cur, dur, min) {
+         const step = time / dur * cur
+         return Math.ceil(step)
+      }
+   }
+})
+// Example 8 Другой угол начала + направление
+const ex_8_render = document.querySelector('.ex-8')
+new ProgressBar(ex_8_render, {
+   current: 99,
+})
+// Example 9 Форматирование текста
+const ex_9_render = document.querySelector('.ex-9')
+const ex_9 = new ProgressBar(ex_9_render, {
+   max: 30,
+   current: 27,
+   text: {
+      formatText: (cur) => {
+         return `${cur}/${ex_9.max}`
+      }
+   }
+})
+ex_9.svg.insertAdjacentHTML('beforeend', `
+<defs>
+    <linearGradient id="grad" x1="0" y1="0" x2="100%" y2="100%">
+        <stop stop-color="steelblue" offset="0" />
+        <stop stop-color="red" offset="100%" />
+    </linearGradient>
+</defs>
+`)
+// Example 9 Форматирование текста
+const ex_10_render = document.querySelector('.ex-10')
+const ex_10 = new ProgressBar(ex_10_render, {
+   max: 5,
+   animation: {
+      trigger: true,
+   }
+})
+ex_10.animation({
+   current: 3,
+   duration: 1000,
+}).then(() => {
+   ex_10.renderElement.insertAdjacentHTML('beforeend', '<p>Конец выполнения</p>')
+})
---
+++ src/ProgressBar.js
@@ -0,0 +1,227 @@
+export default class ProgressBar {
+   // Radius circle
+   #radius = 50
+   // Central position
+   #positionCircle = 50
+   // Size svg
+   #size = 100
+   // Default options
+   static defaultOptions = {
+      min: 0,
+      max: 100,
+      current: 60,
+      clockwise: true,
+      angle: -90,
+      animation: {
+         duration: 2000,
+         trigger: false,
+         cb: (current) => { },
+         timeFunc: (time, current, duration, min) => {
+            current -= min
+            time /= duration
+            return current * time * time + min
+         },
+      },
+      text: {
+         formatText: function (cur) { return `${cur}` }
+      },
+   }
+   // Constructor
+   constructor(renderElement, newOptions) {
+      // If there is no element
+      if (!renderElement)
+         return
+
+      // Render element
+      this.renderElement = renderElement
+      // this.options = Object.assign(ProgressBar.defaultOptions, newOptions)
+      this.options = {
+         ...ProgressBar.defaultOptions, ...newOptions,
+         animation:
+            newOptions?.animation === false ?
+               newOptions?.animation :
+               { ...ProgressBar.defaultOptions.animation, ...newOptions?.animation, },
+         text:
+            newOptions?.text === false ?
+               newOptions?.text :
+               { ...ProgressBar.defaultOptions.text, ...newOptions?.text, },
+      }
+      // Options [min value, max value, current value]
+      this.min = +this.renderElement.dataset.min || this.options.min
+      this.max = +this.renderElement.dataset.max || this.options.max
+      this.current = +this.renderElement.dataset.current || this.options.current
+      // Direction clockwise or counterclockwise (Bool)
+      this.clockwise = this.renderElement.dataset.clockwise || this.options.clockwise
+      // Angle progress
+      this.angle = +this.renderElement.dataset.angle || this.options.angle
+      // Text in circle
+      this.generateText = this.renderElement.hasAttribute('data-generate-text') || this.options.text
+      // Play animation immediately or by trigger
+      this.trigger = this.options.animation?.trigger
+      // Duration animation
+      this.duration = this.renderElement.dataset.duration || this.options.animation?.duration
+      // Timing function for animation
+      this.timeFunc = this.options.animation?.timeFunc
+      // Callback
+      this.cb = this.options.animation?.cb
+      // Function for format text in circle
+      if (this.generateText)
+         this.funcFormatText = this.options.text.formatText
+
+      // Render
+      this.render()
+   }
+   // Progressbar output in render element
+   render() {
+      // Create all svg
+      this.#createSvg()
+      // Paste svg element
+      this.renderElement.append(this.svg)
+      // Add attributes
+      this.#setAttributes(this.svg, {
+         width: this.#size,
+         height: this.#size,
+         viewBox: `0 0 ${this.#size} ${this.#size}`,
+         class: 'progress-bar-svg',
+      })
+      this.#setAttributes(this.track, {
+         cx: this.#positionCircle,
+         cy: this.#positionCircle,
+         r: this.getRadius(this.track),
+      })
+      if (this.text) {
+         this.#setAttributes(this.text, {
+            x: this.#positionCircle,
+            y: this.#positionCircle,
+            'text-anchor': 'middle',
+            dy: '0.346em'
+         })
+      }
+      // Start
+      if (this.trigger)
+         this.setText(this.min)
+      else
+         this.initProgress()
+   }
+   // Set progress
+   initProgress(current = this.current, cb) {
+      this.current = current
+
+      if (this.options.animation) {
+         // For animation
+         this.animation({
+            current,
+            duration: this.duration,
+            cb,
+            timeFunc: this.timeFunc
+         })
+      } else
+         // For static
+         this.setProgress(this.current)
+   }
+   // Update progress value
+   setProgress(current, cb) {
+      // If current >= max || current <= min
+      if (current >= this.max)
+         current = this.max
+      else if (current <= this.min)
+         current = this.min
+
+      // Set progress line
+      this.#setAttributes(this.progress, {
+         d: this.#createPath(
+            this.getRadius(),
+            this.angle,
+            this.#getValidCurrentValue(current),
+            JSON.parse(this.clockwise)
+         ),
+      })
+      // Set text
+      if (this.generateText)
+         this.setText(Math.round(current))
+      // // Calling a function CB
+      if (cb)
+         cb(current)
+   }
+   // Animation
+   animation({ current = this.current, duration = this.duration, cb = this.cb, timeFunc = this.timeFunc }) {
+      return new Promise((resolve) => {
+         // Start of counting
+         const start = new Date()
+         // End of countdown
+         const end = new Date().getTime() + duration
+         // Update
+         const update = () => {
+            const time = Math.min(Date.now(), end) - start
+            this.step = timeFunc(time, current, duration, this.min)
+
+            this.setProgress(this.step, cb)
+            if (time < duration) requestAnimationFrame(update)
+            else resolve()
+         }
+         requestAnimationFrame(update)
+      })
+   }
+   // Set text in tag text
+   setText(current) {
+      this.text.innerHTML = this.funcFormatText(current)
+   }
+   // Get radius
+   getRadius() {
+      return this.#radius - Math.max(parseFloat(this.#getStyle(this.track)), parseFloat(this.#getStyle(this.progress))) / 2;
+   }
+   // Get current value
+   #getValidCurrentValue(value) {
+      return Math.min(360, Math.max(0, (value - this.min) / (this.max - this.min) * 360));
+   }
+   // Create path
+   #createPath(r, startAngle, angle, clockwise) {
+      // Angle less than 0.3 deg create poor trajectories, 
+      // Angle greater than 360 – rollback
+      if (angle > 0 && angle < 0.3)
+         angle = 0
+      else if (angle > 359.999)
+         angle = 359.999
+      // Other settings
+      const endAngle = startAngle + angle * (clockwise * 2 - 1)
+      const startCoords = this.#polarToCartesian(r, startAngle)
+      const endCoords = this.#polarToCartesian(r, endAngle)
+      const x_1 = this.#positionCircle + startCoords.x
+      const y_1 = this.#positionCircle + startCoords.y
+      const x_2 = this.#positionCircle + endCoords.x
+      const y_2 = this.#positionCircle + endCoords.y
+
+      return `M ${x_1} ${y_1} A ${r} ${r} 0 ${+(angle > 180)} ${+clockwise} ${x_2} ${y_2}`
+   }
+   // Convert polar to cartesian
+   #polarToCartesian(r, angle) {
+      return {
+         x: r * Math.cos(angle * Math.PI / 180),
+         y: r * Math.sin(angle * Math.PI / 180)
+      }
+   }
+   // Set attributes
+   #setAttributes(element, attributesObject) {
+      for (const attribute in attributesObject) {
+         const value = attributesObject[attribute];
+         element.setAttribute(attribute, value)
+      }
+   }
+   // Create SVG element
+   #createSvg() {
+      // Create svg
+      this.svg = document.createElementNS("http://www.w3.org/2000/svg", 'svg')
+      // Create circle
+      this.track = document.createElementNS("http://www.w3.org/2000/svg", 'circle')
+      // Create path-element
+      this.progress = document.createElementNS("http://www.w3.org/2000/svg", 'path')
+      // Create text elemetn
+      this.text = this.generateText ? document.createElementNS("http://www.w3.org/2000/svg", 'text') : ''
+      // Paste
+      this.svg.append(this.track, this.progress, this.text)
+   }
+   // # Get computed
+   #getStyle(element) {
+      return parseFloat(window.getComputedStyle(element)['stroke-width']) || 0
+   }
+}
