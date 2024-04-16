636cdbef1d250ab43905b0601999cf77546d6723
Maxim <123332039+sulky-cat@users.noreply.github.com>
2024-04-16 23:00:41 +0300

    Update ProgressBar.js

--- src/ProgressBar.js
+++ src/ProgressBar.js
@@ -145,16 +145,13 @@ export default class ProgressBar {
    }
    // Animation
    animation({ current = this.current, duration = this.duration, cb = this.cb, timeFunc = this.timeFunc }) {
-         // Start of counting
-         const start = new Date()
-         // End of countdown
-         const end = new Date().getTime() + duration
-         // Update
-         return Timer.start(time => {
-            this.step = timeFunc(time, current, duration, this.min)
-            this.setProgress(this.step, cb)
-         }, duration)
-      })
+      const start = new Date()
+      const end = new Date().getTime() + duration
+      // Update
+      return Timer.start(time => {
+         this.step = timeFunc(time, current, duration, this.min)
+          this.setProgress(this.step, cb)
+      }, duration)
    }
    // Set text in tag text
    setText(current) {
